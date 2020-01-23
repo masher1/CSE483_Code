7fa59d704e4bce6edfde6512489d2da77622a040
masher1 <masher@syr.edu>
2020-01-23 00:35:40 -0500

    Adding the project files

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World"); //shortcut, just type . to see what you can do
+
+        }
+    }
+}
---
+++ ConsoleApp/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //DefaultText.Text = "Hello World"; //default text
+            DefaultText.BorderThickness = new Thickness(25);
+        }
+
+        private void DefaultText_MouseEnter(object sender, MouseEventArgs e)
+        {
+            DefaultText.Text = "GET OUT!!!!";
+        }
+
+        private void DefaultText_MouseLeave(object sender, MouseEventArgs e)
+        {
+            DefaultText.Text = "GET IN!!!!";
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DefaultText.Text = "HIIIEEEEEIEE!!";
+        }
+    }
+}
